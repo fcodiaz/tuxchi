a4265ffdd2d8442c372bbc3edb60a34c6bb0a790
Francisco Diaz <franciscod@linux123.(none)>
2010-02-02 17:19:12 -0700

    Se modifico el archivo de comandos en experimental branch

--- src/Comandos.py
+++ src/Comandos.py
@@ -65,8 +65,9 @@ class Comandos:
         Este sera el loop principal del interprete de comandos, basicamente es un loop infinito hasta que se da
         el comando de salida del sistema, de lo contrario, este seguira pidiendo comandos; en caso que algun comando
         falle o la entrada sea incorrecta, el sistema debe reportar el error, no abortarse y continuar con normalidad
-        """
 
+        """
+ # FIXME
         
         
         
